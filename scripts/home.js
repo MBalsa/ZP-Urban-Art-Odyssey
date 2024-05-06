788e72b080149480a3b384f0ac570aba67042285
Milos Sarovic <114680232+Donderion@users.noreply.github.com>
2024-05-06 18:18:02 +0200

    Dodao sam modalne prozore na home stranicu. Izmijenio sam API

--- scripts/home.js
+++ scripts/home.js
@@ -25,3 +25,37 @@
 // });
 
 // ----------------ISPROBAVANJE API-a----------------------IGNORISATI !
+
+const postContainer = document.querySelector(".cardContainer");
+
+const postMethods = () => {
+  fetch("source/data.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      data.images.map((postData) => {
+        const postElement = document.createElement("div");
+        postElement.classList.add("card");
+        postElement.innerHTML = `
+        <img
+          class="card-img"
+          src=${postData.image}
+        />
+        <h2 class="card-headline">${postData.name}</h2>
+        <p class="card-text">${postData.description}</p>
+        <h4>${postData.author}</h4>
+        <input type="button" id="card-button" name="readMore" value="Read more"/>
+      `;
+        postContainer.appendChild(postElement);
+      });
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+};
+
+postMethods();
