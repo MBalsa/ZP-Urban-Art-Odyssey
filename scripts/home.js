5e7ecd805e1d82dab798df53be075fdd8a38ef4f
Milos Sarovic <114680232+Donderion@users.noreply.github.com>
2024-05-09 20:00:18 +0200

    otvaranje i zatvaranje modalnih prozora

--- scripts/home.js
+++ scripts/home.js
@@ -20,7 +20,7 @@ const postMethods = () => {
         <h2 class="card-headline">${postData.name}</h2>
         <p class="card-text">${postData.description}</p>
         <h4>${postData.author}</h4>
-        <input type="button" id="card-button" name="readMore" value="read more"/>
+        <input type="button" id="readMoreBtn" value="read more"/>
       `;
         postContainer.appendChild(postElement);
       });
@@ -45,3 +45,38 @@ loadMoreBtn.onclick = () => {
     loadMoreBtn.style.display = "none";
   }
 };
+
+const readMoreBtn = document.getElementById("readMoreBtn");
+
+const artPrewiev = document.querySelector(".artPreview");
+const modalOverlay = document.querySelector(".modal-overlay");
+
+document.addEventListener("click", function (event) {
+  if (event.target && event.target.id === "readMoreBtn") {
+    artPrewiev.style.display = "block";
+    modalOverlay.style.display = "block";
+  }
+});
+
+const exitBtn = document.querySelector(".exit");
+
+exitBtn.addEventListener("click", function () {
+  artPrewiev.style.display = "none";
+  modalOverlay.style.display = "none";
+});
+
+// const readMoreBtn = document.getElementById("readMoreBtn");
+// const exitBtn = document.getElementById("exit");
+// const artPreview = document.querySelector(".artPreview");
+// const modalOverlay = document.querySelector(".modal-overlay");
+
+// readMoreBtn.addEventListener("click", function () {
+//   artPreview.style.display = "block";
+//   modalOverlay.style.display = "block";
+// });
+// document.addEventListener("DOMContentLoaded", function () {
+//   exitBtn.addEventListener("click", function () {
+//     artPreview.style.display = "none";
+//     modalOverlay.style.display = "none";
+//   });
+// });
